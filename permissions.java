87db204a01ecef9f977851e99fe79a8c5258f857
Ezequiel Alécio Ribeiro <ezequiel.ribeiro@gmail.com>
2022-07-13 15:52:25 -0300

    Create permissions.java

---
+++ permissions.java
@@ -0,0 +1,39 @@
+public static void checkPermission(Activity context) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            List<String> permissions = null;
+            if (context.checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                permissions = new ArrayList<>();
+                permissions.add(Manifest.permission.READ_PHONE_STATE);
+            }
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (permissions == null) {
+                    permissions = new ArrayList<>();
+                }
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (permissions == null) {
+                    permissions = new ArrayList<>();
+                }
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if (context.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (permissions == null) {
+                    permissions = new ArrayList<>();
+                }
+                permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+
+            if (context.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                if (permissions == null) {
+                    permissions = new ArrayList<>();
+                }
+                permissions.add(Manifest.permission.CAMERA);
+            }
+            if (permissions != null) {
+                String[] permissionArray = new String[permissions.size()];
+                permissions.toArray(permissionArray);
+                context.requestPermissions(permissionArray, 0);
+            }
+        }
+    }
